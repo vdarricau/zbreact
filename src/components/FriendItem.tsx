d73c3d4e79e2ea57a858c52a9878048b172e64b7
Valentin Darricau <darricau.valentin@gmail.com>
2023-03-23 00:07:16 +0700

    Extract friend item from friend list

---
+++ src/components/FriendItem.tsx
@@ -0,0 +1,38 @@
+import { Badge, Box, Image, Stack } from "@chakra-ui/react"
+import avatar from "../assets/avatar.png";
+import Friend from "../@ts/Friend";
+import { ReactNode } from 'react';
+
+const FriendItem = ({ children, friend }: { children: ReactNode, friend: Friend }) => {
+    return (
+        <Stack 
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            borderY="1px solid white"
+        >
+            <Image 
+                src={friend.avatar ? friend.avatar : avatar} 
+                alt="friend avatar"
+                h="50px"
+                borderRadius="50"
+            />
+
+            <Box p='6' marginRight="auto">
+                <Box display='flex' alignItems='baseline'>
+                    <Badge borderRadius='full' px='2' colorScheme='teal'>
+                        New
+                    </Badge>
+                    &nbsp;
+                    {friend.username}
+                </Box>
+            </Box>
+
+            <Box p='6' justifySelf="end">
+                {children}
+            </Box>
+        </Stack>
+    )
+}
+
+export default FriendItem;
--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { useAuthHeader } from 'react-auth-kit';
 import Friend from "../@ts/Friend";
 import api from "../api/api";
-import avatar from "../assets/avatar.png";
+import FriendItem from "../components/FriendItem";
 
 const Friends = () => {
     const [ friends, setFriends ] = useState<Array<Friend>>([]);
@@ -40,41 +40,19 @@ const Friends = () => {
     return (
         <>
             <Container>
-                <Stack spacing={6}>
-                    <Heading as='h1' size='4xl' noOfLines={1}>
+                <Box py={5}>
+                    <Heading as='h1' size='2xl' noOfLines={1}>
                         Zbros
                     </Heading>
-                </Stack>
+                </Box>
 
                 { friends.length !== 0 ?  
-                    <SimpleGrid>
+                    <SimpleGrid minChildWidth='320px'>
                         { friends.map((friend) => {
                             return (
-                                <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' key={friend.id}>
-                                    <Image src={friend.avatar ? friend.avatar : avatar} alt="friend avatar" />
-
-                                    <Box p='6'>
-                                        <Box display='flex' alignItems='baseline'>
-                                            <Badge borderRadius='full' px='2' colorScheme='teal'>
-                                                New
-                                            </Badge>
-                                        </Box>
-
-                                        <Box
-                                            mt='1'
-                                            fontWeight='semibold'
-                                            as='h4'
-                                            lineHeight='tight'
-                                            noOfLines={1}
-                                        >
-                                        {friend.username}
-                                        </Box>
-
-                                        <Box display='flex' mt='2' alignItems='center'>
-                                            <Button onClick={() => openSendZbraModal(friend)}>Send Zbra</Button>
-                                        </Box>
-                                    </Box>
-                                </Box>
+                                <FriendItem friend={friend} key={friend.id}>
+                                    <Button borderRadius="50" fontWeight="bold" onClick={() => openSendZbraModal(friend)}>Zbra</Button>
+                                </FriendItem>
                             )
                         })}
                     </SimpleGrid>
