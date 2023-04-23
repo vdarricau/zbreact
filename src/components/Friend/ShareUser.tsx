17ba71f60c9bdd53593bb79e5da514a507179175
Valentin Darricau <darricau.valentin@gmail.com>
2023-04-23 23:19:13 +0530

    Activate prettier on save

--- src/components/Friend/ShareUser.tsx
+++ src/components/Friend/ShareUser.tsx
@@ -1,16 +1,30 @@
-import { Avatar, Box, Heading, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverTrigger, Stack, Text } from "@chakra-ui/react";
-import { FaPaperPlane } from 'react-icons/fa';
+import {
+    Avatar,
+    Box,
+    Heading,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverTrigger,
+    Stack,
+    Text,
+} from "@chakra-ui/react";
+import { FaPaperPlane } from "react-icons/fa";
 import User from "../../@ts/User";
 
-export default function ShareUser({ user }: {user: User}) {
+export default function ShareUser({ user }: { user: User }) {
     const link = "zbra.org/valou";
 
     return (
-        <Popover onOpen={() => {
-            navigator.clipboard.writeText(link);
-        }}>
+        <Popover
+            onOpen={() => {
+                navigator.clipboard.writeText(link);
+            }}
+        >
             <PopoverTrigger>
-                <Stack 
+                <Stack
                     direction="row"
                     alignItems="center"
                     justifyContent="space-between"
@@ -25,16 +39,12 @@ export default function ShareUser({ user }: {user: User}) {
                     <Avatar name={user.username} src={user.avatar} size="sm" />
 
                     <Box px="1" pt="2" pb="1" flex="auto">
-                        <Heading size="sm">
-                            Invite your zbros
-                        </Heading>
-                        <Text fontSize="xs">
-                            { link }
-                        </Text>
+                        <Heading size="sm">Invite your zbros</Heading>
+                        <Text fontSize="xs">{link}</Text>
                     </Box>
 
                     <Box pr="3">
-                        <FaPaperPlane size="18" color="brand.900"/>
+                        <FaPaperPlane size="18" color="brand.900" />
                     </Box>
                 </Stack>
             </PopoverTrigger>
@@ -46,5 +56,5 @@ export default function ShareUser({ user }: {user: User}) {
                 </PopoverBody>
             </PopoverContent>
         </Popover>
-    )
+    );
 }
